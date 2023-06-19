41395ffd996d2fceb25b833df25e9579225fa33f
mkc13 <114617135+mkc13@users.noreply.github.com>
2023-06-19 10:24:30 +0530

    Add files via upload

---
+++ Listen.py
@@ -0,0 +1,19 @@
+import speech_recognition as sr
+
+def Listen():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source,0,5)
+
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio,language="en-in")
+        print(f"You said: {query}\n")
+    except:
+        return ""
+    queerry=str(query)
+    return query.lower()
+
+Listen()
